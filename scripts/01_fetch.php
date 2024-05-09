72edc6fadeef7e87f3438ee0488ae94194235b7e
kiang <kiange@gmail.com>
2024-05-09 18:57:16 +0800

    new way to download data

--- scripts/01_fetch.php
+++ scripts/01_fetch.php
@@ -5,42 +5,49 @@ $basePath = dirname(__DIR__);
 use Symfony\Component\BrowserKit\HttpBrowser;
 use Symfony\Component\HttpClient\HttpClient;
 
+// get files from https://info.nhi.gov.tw/INAE1000/INAE1000S00
+
 $browser = new HttpBrowser(HttpClient::create());
-$csvFile = $basePath . '/raw/list.csv';
+$jsonFile = $basePath . '/raw/map.json';
 
-if (!file_exists($csvFile)) {
-    $json = json_decode(file_get_contents('https://data.gov.tw/api/v2/rest/dataset/39283'), true);
-
-    foreach ($json['result']['distribution'] as $item) {
-        if ($item['resourceFormat'] === 'CSV') {
-            file_put_contents($csvFile, file_get_contents($item['resourceDownloadUrl']));
-            break;
-        }
-    }
+if (!file_exists($jsonFile)) {
+    $browser->jsonRequest('POST', 'https://info.nhi.gov.tw/api/inae1000/INAEmapS01/search', [
+        'datatype' => '1',
+        'keyword' => '',
+        'km' => 20000,
+        'lat' => 24.266060,
+        'lng' => 119.913820,
+    ]);
+    $response = $browser->getResponse()->getContent();
+    $map = json_decode($response, true);
+    file_put_contents($jsonFile, json_encode($map, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+} else {
+    $map = json_decode(file_get_contents($jsonFile), true);
 }
 
 $rawPaths = [];
-if (file_exists($csvFile)) {
-    $fh = fopen($csvFile, 'r');
-    $header = fgetcsv($fh);
-    $header[0] = '醫事機構代碼';
-    while ($line = fgetcsv($fh)) {
-        $data = array_combine($header, $line);
-        if (!isset($rawPaths[$data['縣市別代碼']])) {
-            $rawPaths[$data['縣市別代碼']] = $basePath . '/raw/json/' . $data['縣市別代碼'];
-            if (!file_exists($rawPaths[$data['縣市別代碼']])) {
-                mkdir($rawPaths[$data['縣市別代碼']], 0777, true);
+if (!empty($map['mapdata'])) {
+    foreach ($map['mapdata'] as $item) {
+        $city = mb_substr($item['hosp_addr'], 0, 3, 'utf-8');
+        $pathKey = "{$item['hosp_cnt_type']}/{$city}";
+        if (!isset($rawPaths[$pathKey])) {
+            $rawPaths[$pathKey] = $basePath . "/raw/json/{$item['hosp_cnt_type']}/{$city}";
+            if (!file_exists($rawPaths[$pathKey])) {
+                mkdir($rawPaths[$pathKey], 0777, true);
             }
         }
-        $targetFile = $rawPaths[$data['縣市別代碼']] . '/' . $data['醫事機構代碼'] . '.json';
+        $targetFile = $rawPaths[$pathKey] . '/' . $item['hosp_id'] . '.json';
         if (!file_exists($targetFile)) {
+            echo "{$item['hosp_id']}\n";
             $browser->jsonRequest('POST', 'https://info.nhi.gov.tw/api/inae1000/inae1000s00/SQL300', [
-                'C_HospID' => $data['醫事機構代碼'],
+                'C_HospID' => $item['hosp_id'],
                 'C_planType' => '',
             ]);
             $response = $browser->getResponse()->getContent();
             $detail = json_decode($response, true);
             if (!empty($detail[0])) {
+                $detail[0]['longitude'] = $item['wgs_lon'];
+                $detail[0]['latitude'] = $item['wgs_lat'];
                 file_put_contents($targetFile, json_encode($detail[0], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
             }
         }
