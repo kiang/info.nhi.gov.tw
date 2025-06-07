d9b10a5c6c8bc631a3aaf4b436aa17c5e485e7ff
kiang <kiange@gmail.com>
2025-06-07 08:40:19 +0800

    add headers to prevent blocking

--- scripts/01_fetch_er.php
+++ scripts/01_fetch_er.php
@@ -13,7 +13,24 @@ use Symfony\Component\HttpClient\HttpClient;
 
 // get files from https://info.nhi.gov.tw/INAE4000/INAE4001S02
 
-$browser = new HttpBrowser(HttpClient::create());
+$httpClient = HttpClient::create([
+    'headers' => [
+        'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept' => 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language' => 'zh-TW,zh;q=0.9,en;q=0.8',
+        'Accept-Encoding' => 'gzip, deflate, br',
+        'Referer' => 'https://info.nhi.gov.tw/INAE4000/INAE4001S02',
+        'Origin' => 'https://info.nhi.gov.tw',
+        'X-Requested-With' => 'XMLHttpRequest',
+        'Connection' => 'keep-alive',
+        'Sec-Fetch-Dest' => 'empty',
+        'Sec-Fetch-Mode' => 'cors',
+        'Sec-Fetch-Site' => 'same-origin',
+        'Content-Type' => 'application/json',
+    ]
+]);
+
+$browser = new HttpBrowser($httpClient);
 $rawFile = $basePath . '/raw/er.json';
 
 $browser->jsonRequest('POST', 'https://info.nhi.gov.tw/api/inae4000/inae4001s01/SQL0002', [
